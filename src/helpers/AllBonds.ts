8919ea32418760aab364c2bd3359cbe5c4ba90b4
Omicron DAO <93214657+omic-finance@users.noreply.github.com>
2021-12-16 16:03:18 +0100

    Removed deprecated bonds

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -149,7 +149,7 @@ export const ohm_dai = new LPBond({
 // Is it a stableCoin bond? use `new StableBond`
 // Is it an LP Bond? use `new LPBond`
 // Add new bonds to this array!!
-export const allBonds = [mim, omic_mim, dai, ohm_dai];
+export const allBonds = [mim, omic_mim];
 export const allBondsMap = allBonds.reduce((prevVal, bond) => {
   return { ...prevVal, [bond.name]: bond };
 }, {});
